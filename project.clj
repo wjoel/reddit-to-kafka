e56fa693c320575ae435d42b1d7b5d9ff77597f6
Joel Wilsson <joel.wilsson@gmail.com>
2016-02-23 22:35:39 +0100

    Add license and url to project.clj

--- project.clj
+++ project.clj
@@ -1,5 +1,8 @@
 (defproject com.wjoel/reddit-to-kafka "0.1.0"
   :description "Post reddit data to a Kafka topic"
+  :url "https://github.com/wjoel/reddit-to-kafka"
+  :license {:name "BSD 3-clause"
+            :url "http://opensource.org/licenses/BSD-3-Clause"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/tools.cli "0.3.3"]
                  [org.clojure/tools.logging "0.3.1"]
